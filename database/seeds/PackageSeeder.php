7ba9cfdb679d466bea66db7b0dc4315d6ba3320b
Bonu93 <luca.morucci.kb@gmail.com>
2022-01-24 18:08:29 +0100

    add seed

--- database/seeds/PackageSeeder.php
+++ database/seeds/PackageSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Package;
+use Faker\Generator as Faker;
 
 class PackageSeeder extends Seeder
 {
@@ -10,8 +11,19 @@ class PackageSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 20; $i++) {
+            $newPackage = new Package();
+
+            $newPackage->name = $faker->words(3, true);
+            $newPackage->city = $faker->word();
+            $newPackage->image = $faker->imageUrl(640, 480, true);
+            $newPackage->description = $faker->paragraph(2, true);
+            $newPackage->days = rand(2, 30);
+            $newPackage->price = rand(100, 999);
+
+            $newPackage->save();
+        }
     }
 }
