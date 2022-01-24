376e39aaa1ba2ddb1ceb3a55ec48736a433b97e2
Bonu93 <luca.morucci.kb@gmail.com>
2022-01-24 18:17:38 +0100

    complete

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Package;
 
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index() {
-        return view('home');
+
+        $packages = Package::all();
+        return view('home', compact('packages'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="gallery">
-        
+    <div class="container">
+        <div class="row">
+            @foreach ($packages as $package)
+                <div class="col-2">
+                    <div class="card p-3 my-3">
+                        <img src="{{$package->image}}" alt="">
+                        <h3>{{$package->name}}</h3>
+                        <h4>{{$package->city}}</h4>
+                        <h4>{{$package->days}}</h4>
+                        <span>{{$package->price}}€</span>
+                        <p>{{$package->description}}</p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
